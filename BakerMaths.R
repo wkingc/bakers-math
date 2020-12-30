c1fbf5694b7afe3b1b2fee74d9fea6b3b1e11792
Wade Copeland <wade@kingcopeland.com>
2020-12-30 13:46:40 -0800

    I added in the bakersFormula class and created tests

--- BakerMaths.R
+++ BakerMaths.R
@@ -33,3 +33,45 @@ validStarterObject <- function(object) {
 }
 
 setValidity("bakersStarter", validStarterObject)
+
+new("bakersStarter")
+
+# Class for the formula
+bakersFormula <- setClass(
+  "bakersFormula",
+  
+  slots = c(
+    total_flour_weight = "numeric", # The total flour weight in grams.
+    water_base_percent = "numeric", # The amount of water in the formula.
+    leaven_base_percent = "numeric", # The leaven in the formula.
+    flour_base_names = "character", # The names of each flour type in the starter.
+    flour_base_percent = "numeric" # The percentages of each flour type in the starter.
+  ),
+  
+  prototype = list(
+    total_flour_weight = numeric(),
+    water_base_percent = numeric(),
+    leaven_base_percent = numeric(),
+    flour_base_names = character(),
+    flour_base_percent = numeric()
+  )
+)
+
+validFormulaObject <- function(object) {
+  v1 <- length(object@flour_base_names) == length(object@flour_base_percent)
+  v2 <- sum(object@flour_base_percent) == 100
+  
+  if(v1 & v2) { 
+    return(TRUE)
+  } else if(v1 == FALSE & v2 == TRUE) {
+    return("The number of flours named in the formula and the number of percentage values stated are not the same.")
+  } else if(v1 == TRUE & v2 == FALSE) { 
+    return("Using Baker's Math, the sum of the flours in the formula should be 100%.")
+  } else {
+    return("(1) The number of flours named in the formula and the number of percentage values stated are not the same; (2) Using Baker's Math, the sum of the flours in the formula should be 100%.")
+  }
+}
+
+setValidity("bakersFormula", validFormulaObject)
+
+
--- BakerMathsTest.R
+++ BakerMathsTest.R
@@ -1,7 +1,7 @@
 source("bakerMaths.R")
 
 # Tests for bakersStarter objects
-test_that("Valid bakersStarter object with expected values", {
+test_that("Valid bakersStarter object with expected values.", {
   t1 <- new("bakersStarter",
             mature_starter_percent = 20,
             water_starter_percent = 80,
@@ -16,7 +16,7 @@ test_that("Valid bakersStarter object with expected values", {
 })
 
 
-test_that("An invalid bakersStarter object where the number of flours and number of percentages are not the same", {
+test_that("An invalid bakersStarter object where there are fewer flours than percentages.", {
   expect_error(new("bakersStarter",
                    mature_starter_percent = 20,
                    water_starter_percent = 80,
@@ -26,17 +26,7 @@ test_that("An invalid bakersStarter object where the number of flours and number
                fixed = TRUE)
 })
 
-test_that("An invalid bakersStarter object where the number of flours in the starter are not the same as the number of percentages.", {
-  expect_error(new("bakersStarter",
-                   mature_starter_percent = 20,
-                   water_starter_percent = 80,
-                   flour_starter_names = c("white", "wheat"),
-                   flour_starter_percent = c(80, 15, 5)),
-               "The number of flours named in the starter and the number of percentage values stated are not the same.",
-               fixed = TRUE)
-})
-
-test_that("An invalid bakersStarter object where the number of percentages in the starter are not the same as the number of flours.", {
+test_that("An invalid bakersStarter object where there are fewer percentages than flours.", {
   expect_error(new("bakersStarter",
                    mature_starter_percent = 20,
                    water_starter_percent = 80,
@@ -57,6 +47,54 @@ test_that("An invalid bakersStarter object where the number of flours and percen
                fixed = TRUE)
 })
 
+# Tests for bakersFormula objects
+test_that("Valid bakersFormula object with expected values.", {
+  t1 <- new("bakersFormula",
+            total_flour_weight = 1000,
+            water_base_percent = 78,
+            leaven_base_percent = 12,
+            flour_base_names = c("white", "wheat", "rye"),
+            flour_base_percent = c(80, 15, 5))
+  
+  expect_true(validObject(t1))
+  expect_equal(t1@total_flour_weight, 1000)
+  expect_equal(t1@water_base_percent, 78)
+  expect_equal(t1@leaven_base_percent, 12)
+  expect_equal(t1@flour_base_names, c("white", "wheat", "rye"))
+  expect_equal(t1@flour_base_percent, c(80, 15, 5))
+})
 
 
+test_that("An invalid bakersFormula object where the number of flours is fewer than the number of perentages.", {
+  expect_error(new("bakersFormula",
+                   total_flour_weight = 1000,
+                   water_base_percent = 78,
+                   leaven_base_percent = 12,
+                   flour_base_names = c("white", "wheat"),
+                   flour_base_percent = c(80, 15, 5)),
+               "The number of flours named in the formula and the number of percentage values stated are not the same.",
+               fixed = TRUE)
+})
 
+test_that("An invalid bakersFormula object where the number of fpercentages is less than the number of flours.", {
+  expect_error(new("bakersFormula",
+                   total_flour_weight = 1000,
+                   water_base_percent = 78,
+                   leaven_base_percent = 12,
+                   flour_base_names = c("white", "wheat", "rye"),
+                   flour_base_percent = c(80, 20)),
+               "The number of flours named in the formula and the number of percentage values stated are not the same.",
+               fixed = TRUE)
+})
+
+
+test_that("An invalid bakersFormula object where the number of flours and percentages are not the same, and the percentages do not sum to 100%", {
+  expect_error(new("bakersFormula",
+                   total_flour_weight = 1000,
+                   water_base_percent = 78,
+                   leaven_base_percent = 12,
+                   flour_base_names = c("white", "wheat"),
+                   flour_base_percent = c(80, 15, 20)),
+               "(1) The number of flours named in the formula and the number of percentage values stated are not the same; (2) Using Baker's Math, the sum of the flours in the formula should be 100%.",
+               fixed = TRUE)
+})
