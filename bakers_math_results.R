4e42a51c142fc194ce83a565c6df7d45cf5ece37
Wade C <wade@kingcopeland.com>
2025-06-09 19:20:03 -0400

    Updated the formula.

--- bakers_math_results.R
+++ bakers_math_results.R
@@ -14,14 +14,14 @@ starter_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water <-
         flour_starter_names = c("All purpose white flour", "Whole wheat flour"),
         flour_starter_percent = c(80, 20))
 
-#############################################
-# Waimee's Levained Sandwich Bread (1.0 kg) #
-#############################################
-waimee_levain_sandwich_bread_1.0kg <-
+####################################
+# Waimee's Levained Sandwich Bread #
+####################################
+waimee_levain_sandwich_bread <-
     new(
         "bakersFormula",
-        formula_name = "Waimee's Sandwich Bread (1.0 kg)",
-        total_flour_weight = 1000,
+        formula_name = "Waimee's Sandwich Bread",
+        total_flour_weight = 500,
         water_base_percent = 78,
         levain_base_percent = 12,
         flour_base_names = c("All purpose white flour", "Whole wheat flour"),
@@ -29,59 +29,29 @@ waimee_levain_sandwich_bread_1.0kg <-
         other_names = c("Fine sea salt"),
         other_percent = c(2.2), 
         notes = 
-            "1) Feed your starter in the evening and keep it at room temperatue (about 70 degrees) overnight.  In the morning it should have at least doubled in size from the build-up of gas.  If the starter smells like over-ripe fruit you will end up with a more buttery flavour.  If the starter goes longer, it will begin to have a vinergary smell, and the final loaf will be more sour.
+            "1) Feed your starter with 80 degree water and keep it at room temperatue for about 8 hours until it has doubled in size from the build-up of gas.  If the starter smells like over-ripe fruit you will end up with a more buttery flavour.  If the starter goes longer, it will begin to have a vinergary smell, and the final loaf will be more sour.
         2) Mix the flour and 90-95 degree water in a tub and let it autolyse for about 30 minutes before incorporating the levain and salt.  The target temperature of the dough at the end of the final mix is 77-78 degrees. 
-        3) Bulk ferment the dough until it has at most doubled in volume (about 8 hours later in a 70-degree kitchen).  Unlike with baker's yeast, if you allow the bulk ferment to go on for too long, the acidic starter will begin to break down the gluten network in the dough.  Build up the dough's gluten structure by performing four stretch-and-folds at 30 minute intervals during the bulk ferment's first two hours. 
-        4) After the bulk ferment, preshape the dough into a two equal size rounds with a slight amount of tension, and let them bench-rest for 15 minutes.
-        5) Create a sausage-roll loaf with tension by pushing it back as you roll it (https://www.youtube.com/watch?v=jIzKcred_lc).  Put the loaves seam-side down in a greased 2 lb loaf pans and dust with flour.  Loosely cover each loaf pan with plastic (a disposable plastic shower cap works well) and put in a refrigerator to develop for 12-14 hours.
+        3) Bulk ferment the dough until it has not quite doubled in volume.  Build up the dough's gluten structure by performing four stretch-and-folds at 30 minute intervals during the bulk ferment's first two hours. 
+        4) After the bulk ferment, preshape the dough into a round with a slight amount of tension, and let it bench-rest for 15 minutes.
+        5) Create a sausage-roll loaf with tension by pushing it back as you roll it (see https://www.youtube.com/watch?v=jIzKcred_lc for shaping technique).  Put the loaf seam-side down in a greased 2 lb loaf pan and dust with flour.  Loosely cover each loaf pan (a disposable plastic shower cap works well!) and put in a refrigerator to proof for 12-14 hours.
         7) Preheat your oven to 400 degrees and remove the loaf pans from the refrigerator.  By the time the loaves are ready to bake, the dough should have just crested above the top of each pan.
         8) Bake the loaves for about 40 minutes until the tops are medium brown.
         8) After removing the loaves from the oven, and while still in the loaf pans, brush the tops with butter.  This step will help keep the crust from getting too hard as it cools.
-        9) Remove each loaf from their tin and place on a cooling rack for a minimum of two hours, but preferably 3-4 hours, before slicing.")
+        9) Remove each loaf from their tin and place on a cooling rack for a minimum of one hour before slicing.")
 
-waimee_levain_sandwich_bread_1.0kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water <- bakers_math(starter_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, waimee_levain_sandwich_bread_1.0kg)
+waimee_levain_sandwich_bread_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water <- bakers_math(starter_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, waimee_levain_sandwich_bread)
 
-save_kable(waimee_levain_sandwich_bread_1.0kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, file = "./formula/waimee_levain_sandwich_loaf_1.0kg.pdf")
+save_kable(waimee_levain_sandwich_bread_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, file = "./formula/waimee_levain_sandwich_loaf.pdf")
 
-waimee_levain_sandwich_bread_1.0kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water
+waimee_levain_sandwich_bread_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water
 
-###########################################
-# Waimee's Levained Hearth Bread (1.0 kg) #
-###########################################
-waimee_levain_hearth_bread_1.0kg <-
+##################################
+# Waimee's Levained Hearth Bread #
+##################################
+waimee_levain_hearth_bread <-
     new(
         "bakersFormula",
-        formula_name = "Waimee's Hearth Bread (1 kg)",
-        total_flour_weight = 1000,
-        water_base_percent = 78,
-        levain_base_percent = 12,
-        flour_base_names = c("All purpose white flour", "Whole wheat flour"),
-        flour_base_percent = c(90, 10),
-        other_names = c("Fine sea salt"),
-        other_percent = c(2.2),
-        notes = 
-            "1) Feed your starter in the evening and keep it at room temperatue (about 70 degrees) overnight.  In the morning it should have doubled in size from the build-up of gas.  If the starter smells like over-ripe fruit you will end up with a more buttery flavour.  If the starter goes longer, it will begin to have a vinergary smell, and the final loaf will be more sour.
-         2) Mix the flour and 90-95 degree water in a tub and let it autolyse for about 30 minutes before incorporating the levain and salt.  The target temperature of the dough at the end of the final mix is 77-78 degrees. 
-         3) Bulk ferment the dough until it has doubled in volume (about 8 hours later in a 70-degree kitchen).  Build up the dough's gluten structure by performing four stretch-and-folds at 30 minute intervals during the bulk ferment's first two hours.
-         4) After the bulk ferment, preshape the dough into two rounds with a slight amount of tension, and let them bench-rest for 15 minutes.
-         5) Create two batards or boules (https://www.youtube.com/watch?v=O6INjgO91mg&t=130s) and place them seam-side up in seasoned proofing baskets.  Loosely cover them with plastic and let the dough proof overnight in the fridge for 12-15 hours.
-         6) Preheat your cast-iron baking vessel (https://challengerbreadware.com/product/challenger-bread-pan/) in the oven for an hour at 500 degrees.  Place your proofed bread in the baking vessel and score.  If there is enough room without touching the bread, put in an ice cube or two for extra steam.
-         7) Place the baking vessel in the oven and adjust to 435 degrees.  Bake for 15 minutes.
-         8) After 15 minutes remove the baking vessel cover and bake for another 30 minutes, or until your desired crust color is achieved.")
-
-waimee_levain_hearth_bread_1.0kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water <- bakers_math(starter_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, waimee_levain_hearth_bread_1.0kg)
-
-save_kable(waimee_levain_hearth_bread_1.0kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, file = "./formula/waimees_levain_hearth_bread_1.0kg.pdf")
-
-waimee_levain_hearth_bread_1.0kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water
-
-###########################################
-# Waimee's Levained Hearth Bread (0.5 kg) #
-###########################################
-waimee_levain_hearth_bread_0.5kg <-
-    new(
-        "bakersFormula",
-        formula_name = "Waimee's Hearth Bread (0.5 kg)",
+        formula_name = "Waimee's Hearth Bread",
         total_flour_weight = 500,
         water_base_percent = 78,
         levain_base_percent = 12,
@@ -90,50 +60,48 @@ waimee_levain_hearth_bread_0.5kg <-
         other_names = c("Fine sea salt"),
         other_percent = c(2.2),
         notes = 
-            "1) Feed your starter in the evening and keep it at room temperatue (about 70 degrees) overnight.  In the morning it should have doubled in size from the build-up of gas.  If the starter smells like over-ripe fruit you will end up with a more buttery flavour.  If the starter goes longer, it will begin to have a vinergary smell, and the final loaf will be more sour.
+            "1) Feed your starter with 80 degree water and keep it at room temperatue for about 8 hours until it has doubled in size from the build-up of gas.  If the starter smells like over-ripe fruit you will end up with a more buttery flavour.  If the starter goes longer, it will begin to have a vinergary smell, and the final loaf will be more sour.
          2) Mix the flour and 90-95 degree water in a tub and let it autolyse for about 30 minutes before incorporating the levain and salt.  The target temperature of the dough at the end of the final mix is 77-78 degrees. 
-         3) Bulk ferment the dough until it has doubled in volume (about 8 hours later in a 70-degree kitchen).  Build up the dough's gluten structure by performing four stretch-and-folds at 30 minute intervals during the bulk ferment's first two hours.
-         4) After the bulk ferment, preshape the dough into two rounds with a slight amount of tension, and let them bench-rest for 15 minutes.
-         5) Create one batard or boule (https://www.youtube.com/watch?v=O6INjgO91mg&t=130s) and place it seam-side up in seasoned proofing baskets.  Loosely cover it with plastic and let the dough proof overnight in the fridge for 12-15 hours.
-         6) Preheat your cast-iron baking vessel (https://challengerbreadware.com/product/challenger-bread-pan/) in the oven for an hour at 500 degrees.  Place your proofed bread in the baking vessel and score.  If there is enough room without touching the bread, put in an ice cube or two for extra steam.
-         7) Place the baking vessel in the oven and adjust to 435 degrees.  Bake for 15 minutes.
-         8) After 15 minutes remove the baking vessel cover and bake for another 30 minutes, or until your desired crust color is achieved.")
+         3) Bulk ferment the dough until it has not quite doubled in volume.  Build up the dough's gluten structure by performing four stretch-and-folds at 30 minute intervals during the bulk ferment's first two hours.
+         4) After the bulk ferment, preshape the dough into a round with a slight amount of tension, and let it bench-rest for 15 minutes.
+         5) Create a batard or boule (see https://www.youtube.com/watch?v=O6INjgO91mg&t=130s for shaping technique) and place it seam-side up in 50% flour/50% rice flour seasoned proofing basket.  Loosely cover the proofing basket (a disposable plastic shower cap works well!) and put in a refrigerator to proof for 12-14 hours.
+         6) Preheat your cast-iron baking vessel (see https://challengerbreadware.com/product/challenger-bread-pan/ for a recommendation) in the oven for an hour at 450 degrees.  Place your proofed bread in the baking vessel and score.  If there is enough room without touching the bread, put in an ice cube for extra steam.  Bake for 20 minutes.
+         7) After 20 minutes remove the baking vessel cover and bake for another 25 minutes, or until your desired crust color is achieved.")
 
-waimee_levain_hearth_bread_0.5kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water <- bakers_math(starter_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, waimee_levain_hearth_bread_0.5kg)
+waimee_levain_hearth_bread_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water <- bakers_math(starter_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, waimee_levain_hearth_bread)
 
-save_kable(waimee_levain_hearth_bread_0.5kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, file = "./formula/waimee_levain_hearth_bread_0.5kg.pdf")
+save_kable(waimee_levain_hearth_bread_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, file = "./formula/waimees_levain_hearth_bread.pdf")
 
-waimee_levain_hearth_bread_0.5kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water
+waimee_levain_hearth_bread_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water
 
-#####################################################
-# Waimee's Overnight Levained Hearth Bread (0.5 kg) #
-#####################################################
-waimee_overnight_levain_hearth_bread_0.5kg <-
+############################################
+# Waimee's Overnight Levained Hearth Bread #
+############################################
+waimee_overnight_levain_hearth_bread <-
     new(
         "bakersFormula",
-        formula_name = "Waimee's Overnight Hearth Bread (0.5 kg)",
+        formula_name = "Waimee's Overnight Hearth Bread",
         total_flour_weight = 500,
         water_base_percent = 78,
-        levain_base_percent = 6,
+        levain_base_percent = 4,
         flour_base_names = c("All purpose white flour", "Whole wheat flour"),
         flour_base_percent = c(90, 10),
         other_names = c("Fine sea salt"),
         other_percent = c(2.2),
         notes = 
-            "1) Feed your starter in the morning and keep it at room temperatue (about 70 degrees) overnight.  In the afternoon it should have doubled in size from the build-up of gas.  If the starter smells like over-ripe fruit you will end up with a more buttery flavour.  If the starter goes longer, it will begin to have a vinergary smell, and the final loaf will be more sour.
+            "1) Feed your starter with 70 degree water and keep it at room temperatue for about 11 hours until it has doubled in size from the build-up of gas.  If the starter smells like over-ripe fruit you will end up with a more buttery flavour.  If the starter goes longer, it will begin to have a vinergary smell, and the final loaf will be more sour.
          2) Mix the flour and 90-95 degree water in a tub and let it autolyse for about 30 minutes before incorporating the levain and salt.  The target temperature of the dough at the end of the final mix is 77-78 degrees. 
-         3) Bulk ferment the dough until it has doubled in volume (about 13 hours later in a 70-degree kitchen).  Build up the dough's gluten structure by performing four stretch-and-folds at 30 minute intervals during the bulk ferment's first two hours.
-         4) After the bulk ferment, preshape the dough into two rounds with a slight amount of tension, and let them bench-rest for 15 minutes.
-         5) Create one batard or boule (https://www.youtube.com/watch?v=O6INjgO91mg&t=130s) and place it seam-side up in seasoned proofing baskets.  Loosely cover it with plastic and let the dough proof overnight in the fridge for 12-15 hours.
-         6) Preheat your cast-iron baking vessel (https://challengerbreadware.com/product/challenger-bread-pan/) in the oven for an hour at 500 degrees.  Place your proofed bread in the baking vessel and score.  If there is enough room without touching the bread, put in an ice cube or two for extra steam.
-         7) Place the baking vessel in the oven and adjust to 435 degrees.  Bake for 15 minutes.
-         8) After 15 minutes remove the baking vessel cover and bake for another 30 minutes, or until your desired crust color is achieved.")
+         3) Bulk ferment the dough until it has not quite doubled in volume.  Build up the dough's gluten structure by performing four stretch-and-folds at 30 minute intervals during the bulk ferment's first two hours.
+         4) After the bulk ferment, preshape the dough into a round with a slight amount of tension, and let it bench-rest for 15 minutes.
+         5) Create a batard or boule (see https://www.youtube.com/watch?v=O6INjgO91mg&t=130s shaping technique) and place it seam-side up in a 50% flour/50% rice flour seasoned proofing basket.  Loosely cover the proofing basket (a disposable plastic shower cap works well!) and put in a refrigerator to proof for 12-14 hours.
+         6) Preheat your baking vessel (see https://challengerbreadware.com/product/challenger-bread-pan/ for my recommendation) in the oven for an hour at 450 degrees.  Place your proofed bread in the baking vessel and score.  If there is enough room without touching the bread, put in an ice cube for extra steam.  Bake for 20 minutes.
+         8) After 20 minutes remove the baking vessel cover and bake for another 25 minutes, or until your desired crust color is achieved.")
 
-waimee_overnight_levain_hearth_bread_0.5kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water <- bakers_math(starter_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, waimee_overnight_levain_hearth_bread_0.5kg)
+waimee_overnight_levain_hearth_bread_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water <- bakers_math(starter_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, waimee_overnight_levain_hearth_bread)
 
-save_kable(waimee_overnight_levain_hearth_bread_0.5kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, file = "./formula/waimee_overnight_levain_hearth_bread_0.5kg.pdf")
+save_kable(waimee_overnight_levain_hearth_bread_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water, file = "./formula/waimee_overnight_levain_hearth_bread.pdf")
 
-waimee_overnight_levain_hearth_bread_0.5kg_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water
+waimee_overnight_levain_hearth_bread_1pt_mature_1.6pt_all_purpose_white_0.4pt_whole_wheat_2pt_water
 
 ########################################
 # Waimee's Pizza Dough (3 Dough Balls) #
@@ -145,8 +113,8 @@ waimees_pizza_dough_3balls <-
         total_flour_weight = 500,
         water_base_percent = 70,
         levain_base_percent = 25,
-        flour_base_names = c("Caputo 00 flour", "All purpose white flour", "Whole wheat flour"),
-        flour_base_percent = c(75, 20, 5),
+        flour_base_names = c("All purpose white flour", "Whole wheat flour"),
+        flour_base_percent = c(90, 10),
         other_names = "Fine sea salt",
         other_percent = 2.8,
         notes = 
